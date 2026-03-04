61e1d7f18e7751e712a1422140f1dc490ec7f737
Ivan Prykhodko <i.m.prykhodko@student.itc.khai.edu>
2026-03-04 20:47:42 +0200

    all done

--- task4/src/App.js
+++ task4/src/App.js
@@ -2,28 +2,44 @@ import React, { useState } from "react";
 
 function FormWithValidation() {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("")
   const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!email.includes("@")) {
-      setError("Некоректний email!");
-    } else {
-      setError("");
-      alert("Форма успішно відправлена!");
+    if (!name || !email) {
+      setError("Please fill in all fields");
+      return;
+    } if (!email.includes("@")) {
+      setError("Incorrect email");
+      return;
     }
+
+    setError("");
+    alert("Form was sent successfully");
+
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <div>
+            Name:
+            <input
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
         </label>
         {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
