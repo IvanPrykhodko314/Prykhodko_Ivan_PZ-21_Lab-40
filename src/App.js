475608357f617df496ac556c96da4fb9b419b709
Ivan Prykhodko <i.m.prykhodko@student.itc.khai.edu>
2026-03-04 19:55:48 +0200

    First task done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function ControlledForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [pass, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,6 +29,17 @@ function ControlledForm() {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Password:
+          <input
+            type="pass"
+            value={pass}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
       </div>
